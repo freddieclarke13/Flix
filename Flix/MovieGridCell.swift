e226b80ef36d692deb7075338d8e9d23736400c3
freddieclarke13 <freddieclarke13@gmail.com>
2021-02-09 13:31:50 -0500

    Added superhero tab

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Freddie Clarke on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
